a292989cba838c01abd1d773485e55f9fcd8cb11
Yasunari_Kainuna <yasu6491@icloud.com>
2021-04-10 21:51:28 +0900

    [Add]js.erb読み込むためにrails-erb-loader追加

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,5 +1,6 @@
 const { environment } = require('@rails/webpacker')
 const webpack = require('webpack')
+const erb = require('./loaders/erb')
 
 environment.plugins.prepend('Provide',
     new webpack.ProvidePlugin({
@@ -8,4 +9,5 @@ environment.plugins.prepend('Provide',
     })
 )
 
+environment.loaders.prepend('erb', erb)
 module.exports = environment
