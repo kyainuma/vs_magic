cea9342ea226f7dc6c31c31acccf3e534d72c6ef
Yasunari_Kainuna <yasu6491@icloud.com>
2021-04-17 17:28:13 +0900

    [Fix]Rails.env.production? ? "RANDOM()" : "rand()"

--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -1,7 +1,8 @@
 class ChallengesController < ApplicationController
   def index
     # @shortcut_keys = ShortcutKey.where(id: 75)
-    @shortcut_keys = ShortcutKey.order(Arel.sql('RAND()')).limit(10)
+    rand = Rails.env.production? ? 'RANDOM()' : 'rand()'
+    @shortcut_keys = ShortcutKey.order(Arel.sql(rand).limit(10))
     @shortcut_keys_json = @shortcut_keys.to_json
   end
 end
