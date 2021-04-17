efacff9a05499463f22d8ff4e53dfdd1facef4f0
Yasunari_Kainuma <70623977+kyainuma@users.noreply.github.com>
2021-04-17 21:29:25 +0900

    Revert "[Fix]Rails.env.production? ? "RANDOM()" : "rand()""

--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -1,8 +1,7 @@
 class ChallengesController < ApplicationController
   def index
     # @shortcut_keys = ShortcutKey.where(id: 75)
-    rand = Rails.env.production? ? 'RANDOM()' : 'rand()'
-    @shortcut_keys = ShortcutKey.order(Arel.sql(rand).limit(10))
+    @shortcut_keys = ShortcutKey.order(Arel.sql('RAND()')).limit(10)
     @shortcut_keys_json = @shortcut_keys.to_json
   end
 end
