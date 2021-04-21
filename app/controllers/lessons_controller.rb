0890d05886f6fc1162eb58a9d24d41f4d5ecf9a1
Yasunari_Kainuna <yasu6491@icloud.com>
2021-04-21 13:28:43 +0900

    [Fix]ルーティング修正

---
+++ app/controllers/lessons_controller.rb
@@ -0,0 +1,19 @@
+class LessonsController < ApplicationController
+  def index
+    @shortcut_keys = if request.os == 'Mac OSX'
+                       MacShortcutKey.all
+                     else
+                       WinShortcutKey.all
+                     end
+    @shortcut_keys_json = @shortcut_keys.to_json
+  end
+
+  def show
+    @shortcut_key = if request.os == 'Mac OSX'
+                      MacShortcutKey.find(params[:id])
+                    else
+                      WinShortcutKey.find(params[:id])
+                    end
+    @shortcut_key_json = @shortcut_key.to_json
+  end
+end
--- app/controllers/time_attacks_controller.rb
+++ app/controllers/time_attacks_controller.rb
@@ -1,3 +1,10 @@
 class TimeAttacksController < ApplicationController
-  def show; end
+  def index
+    @shortcut_keys = if request.os == 'Mac OSX'
+                       MacShortcutKey.order(Arel.sql('RAND()')).limit(10)
+                     else
+                       WinShortcutKey.order(Arel.sql('RAND()')).limit(10)
+                     end
+    @shortcut_keys_json = @shortcut_keys.to_json
+  end
 end
