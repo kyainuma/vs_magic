c3fc401cce1589d87ab14f40b7c68b26bfa48aee
Yasunari_Kainuna <yasu6491@icloud.com>
2021-04-22 20:44:21 +0900

    [Fix]デザイン等修正

--- app/controllers/challenges_controller.rb
+++
@@ -1,7 +0,0 @@
-class ChallengesController < ApplicationController
-  def index
-    # @shortcut_keys = ShortcutKey.where(id: 75)
-    @shortcut_keys = ShortcutKey.order(Arel.sql('RAND()')).limit(10)
-    @shortcut_keys_json = @shortcut_keys.to_json
-  end
-end
