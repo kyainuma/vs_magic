baa0b16789f3101bf35db650494c9c56519dde43
Yasunari_Kainuna <yasu6491@icloud.com>
2021-04-23 05:16:13 +0900

    [Fix]マイグレファイル修正

--- db/migrate/20210326235122_create_mac_shortcut_keys.rb
+++ db/migrate/20210326235122_create_mac_shortcut_keys.rb
@@ -1,6 +1,6 @@
-class CreateShortcutKeys < ActiveRecord::Migration[6.0]
+class CreateMacShortcutKeys < ActiveRecord::Migration[6.0]
   def change
-    create_table :shortcut_keys do |t|
+    create_table :mac_shortcut_keys do |t|
       t.string :answer_key, null: false
       t.string :modifier_key
       t.string :question, null: false
