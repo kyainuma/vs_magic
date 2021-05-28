118cc9bbb3d2a95de0109abbfe41a58264ce7682
Yasunari_Kainuna <yasu6491@icloud.com>
2021-05-28 16:31:18 +0900

    [Fix]herokuデプロイ時'ArgumentError: wrong number of arguments (given 0, expected 1+)'修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,9 +3,6 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
-  config.storage :fog
-  config.fog_provider = 'fog/aws'
-  config.fog_directory = Rails.application.credentials.dig[:s3_fog_directory]
   config.fog_credentials = {
     provider: 'AWS',
     aws_access_key_id: Rails.application.credentials.dig(:aws, :access_key_id),
@@ -13,4 +10,6 @@ CarrierWave.configure do |config|
     region: 'ap-northeast-1',
     path_style: true
   }
+
+  config.fog_directory = Rails.application.credentials.dig(:s3_fog_directory)
 end
