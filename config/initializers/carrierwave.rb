af60866c39b4acb194bdd7f5c93ee3113a7a78f7
Yasunari_Kainuna <yasu6491@icloud.com>
2021-05-28 16:21:04 +0900

    [Add]credentialsのAWS S３の認証情報をcarrierwaveで読み込み

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,11 +5,11 @@ require 'carrierwave/storage/fog'
 CarrierWave.configure do |config|
   config.storage :fog
   config.fog_provider = 'fog/aws'
-  config.fog_directory = 'vsmagic'
+  config.fog_directory = Rails.application.credentials.dig[:s3_fog_directory]
   config.fog_credentials = {
     provider: 'AWS',
-    aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+    aws_access_key_id: Rails.application.credentials.dig(:aws, :access_key_id),
+    aws_secret_access_key: Rails.application.credentials.dig(:aws, :secret_access_key),
     region: 'ap-northeast-1',
     path_style: true
   }
