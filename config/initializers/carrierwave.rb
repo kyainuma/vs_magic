1a6232d3b963328684e4e439de07e036f3b2fcde
Yasunari_Kainuna <yasu6491@icloud.com>
2021-05-28 16:45:34 +0900

    [Fix]seed_fu実行時'Excon::Error::Socket: Broken pipe (Errno::EPIPE)'修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,13 +3,15 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
+  config.storage = :fog
+  config.fog_provider = 'fog/aws'
   config.fog_credentials = {
     provider: 'AWS',
-    aws_access_key_id: Rails.application.credentials.dig(:aws, :access_key_id),
-    aws_secret_access_key: Rails.application.credentials.dig(:aws, :secret_access_key),
-    region: 'ap-northeast-1',
-    path_style: true
+    aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
+    aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
+    region: 'ap-northeast-1'
   }
 
-  config.fog_directory = Rails.application.credentials.dig(:s3_fog_directory)
+  config.fog_directory  = 'vsmagic'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/vsmagic'
 end
