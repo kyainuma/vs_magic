a14a2929b5b7d081b35dbcc2dd6029ed422ebe4b
Yasunari_Kainuna <yasu6491@icloud.com>
2021-05-30 15:57:32 +0900

    [Add]TimeAttack, Lesson, Rankingテスト追加

--- spec/factories/shortcut_keys.rb
+++ spec/factories/shortcut_keys.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :shortcut_key do
-    answer_key { 'test_answer_key' }
-    modifier_key { 'test_modifier_key' }
-    question { 'test_question' }
-    display_key { 'test_display_key' }
+    sequence(:answer_key) { |n| "test_answer_key_#{n}" }
+    sequence(:modifier_key) { |n| "test_modifier_key_#{n}" }
+    sequence(:question) { |n| "test_question_#{n}" }
+    sequence(:display_key) { |n| "test_display_key_#{n}" }
     os_type { 0 }
     image { File.new(Rails.root.join('public/no_image.gif')) }
   end
