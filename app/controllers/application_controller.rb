632e3bd3253d768c27f50e9d7cf695033428abde
Yasunari_Kainuna <yasu6491@icloud.com>
2021-06-02 13:55:45 +0900

    [Add]rails_admin設定追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,6 @@ class ApplicationController < ActionController::Base
 
     def not_authenticated
       flash[:warning] = t('defaults.message.require_login')
-      redirect_to login_path
+      redirect_to main_app.login_path
     end
 end
