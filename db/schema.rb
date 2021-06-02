2107766cfc6d6769eb6ac4a8eb2491509157e6c6
Yasunari_Kainuna <yasu6491@icloud.com>
2021-06-02 13:50:46 +0900

    [Add]ユーザー権限カラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_01_013447) do
+ActiveRecord::Schema.define(version: 2021_06_02_021140) do
 
   create_table "contacts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "email", null: false
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 2021_06_01_013447) do
     t.datetime "reset_password_token_expires_at"
     t.datetime "reset_password_email_sent_at"
     t.integer "access_count_to_reset_password_page", default: 0
+    t.integer "role", default: 0, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
